400bfa5ba2dcac39f177c1b6bd2c2740f637b1aa
Joseph Moffatt <joseph.moffatt@gmail.com>
2025-04-05 00:29:35 +0100

    Prepare page and styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,108 @@
-import { View, Text } from "react-native";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { useBottomTabOverflow } from "@/components/ui/TabBarBackground.ios";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "nativewind";
+import { SectionList, Text, TouchableHighlight, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const CLUBS = [
+  {
+    title: "Woods",
+    data: ["Driver", "3 Wood", "5 Wood", "7 Wood", "9 Wood"],
+  },
+  {
+    title: "Hybrids",
+    data: [
+      "1 Hybrid",
+      "2 Hybrid",
+      "3 Hybrid",
+      "4 Hybrid",
+      "5 Hybrid",
+      "6 Hybrid",
+      "7 Hybrid",
+      "8 Hybrid",
+      "9 Hybrid",
+    ],
+  },
+  {
+    title: "Irons",
+    data: [
+      "1 Iron",
+      "2 Iron",
+      "3 Iron",
+      "4 Iron",
+      "5 Iron",
+      "6 Iron",
+      "7 Iron",
+      "8 Iron",
+      "9 Iron",
+    ],
+  },
+  {
+    title: "Wedges",
+    data: [
+      "44 Wedge",
+      "45 Wedge",
+      "46 Wedge",
+      "47 Wedge",
+      "48 Pitching Wedge",
+      "50 Wedge",
+      "52 Wedge",
+      "53 Wedge",
+      "54 Wedge",
+      "55 Wedge",
+      "56 Sand Wedge",
+      "58 Wedge",
+      "60 Lob Wedge",
+      "62 Wedge",
+      "64 Wedge",
+    ],
+  },
+];
 
 export default function PrepareScreen() {
+  const { colorScheme } = useColorScheme();
+  const bottom = useBottomTabOverflow();
+  const onPress = () => console.log("Pressed");
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-white text-xl">Prepare</Text>
-    </View>
+    <SafeAreaView className="bg-neutral-100 dark:bg-neutral-950 flex-1">
+      <SectionList
+        sections={CLUBS}
+        keyExtractor={(item, index) => item + index}
+        stickySectionHeadersEnabled={false}
+        contentContainerStyle={{
+          paddingBottom: bottom,
+        }}
+        renderItem={({ item, index, section }) => (
+          <TouchableHighlight
+            underlayColor={
+              Colors[colorScheme ?? "light"].touchableHighlightUnderlayColor
+            }
+            className={`${index === 0 ? "rounded-t-xl" : ""} ${
+              index === section.data.length - 1 ? "rounded-b-xl" : ""
+            } bg-white dark:bg-neutral-800`}
+            onPress={onPress}
+          >
+            <View
+              className={`flex flex-row justify-between items-center ${
+                index !== section.data.length - 1
+                  ? "border-b border-neutral-200 dark:border-neutral-600"
+                  : ""
+              } ml-5 py-3 pr-5 bg-ne`}
+            >
+              <Text className="text-lg dark:text-white">{item}</Text>
+              <IconSymbol name="checkmark" color="text-white" size={16} />
+            </View>
+          </TouchableHighlight>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-sm uppercase ml-5 mt-10 mb-2 text-neutral-400 dark:text-neutral-600">
+            {title}
+          </Text>
+        )}
+        className="px-5 mb-5"
+      />
+    </SafeAreaView>
   );
 }
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,10 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    tabBarActiveTintColor: "#0ea5e9", // bg-sky-500
+    touchableHighlightUnderlayColor: "#e5e5e5", // bg-neutral-200
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    tabBarActiveTintColor: "#0ea5e9", // bg-sky-500
+    touchableHighlightUnderlayColor: "#404040", // bg-neutral-800
   },
 };
